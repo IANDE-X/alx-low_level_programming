de73124cab481aebbdd4738aa771a2c4c9d4a01f
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-01 01:00:45 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,13 @@
-#include<studio.h>
-#include<unistd.h>
-/**                                                                                                                                     
- * main - Entry point  
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
  *
- * Description: "and that piece of art is useful" - Dora Korpar, 2015-10-19"
- *                                                                                                                                      
- * *Return: Always 0 (success)                                                                                                          
- */      
+ * Return: Always 1 (Error)
+ */
+
 int main(void)
 {
-  
- /**                                                                                                                                    
-   * Return something otherwise return 0                                                                                               
-   */ 
-  
-   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-   return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
