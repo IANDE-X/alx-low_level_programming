d2d940d259f1f2ab385128c3791e22ad4b9d8106
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-01 00:14:53 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,18 @@
-#include <stdio.h>                                                                                                                      
-                                                                                                                                        
+#include <stdio.h>                                                                                                                                                                                                                                          
 /**                                                                                                                                     
-* main - Prints "with proper grammar, but the outcome is a piece of art,"                                                              
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"                                                              
  *                                                                                                                                      
  * *Return: Always 0 (success)                                                                                                          
- */                                                                                                                                     
-                                                                                                                                        
+ */                                                                                                                                                                                                                                                                         
 int main(void)                                                                                                                          
                                                                                                                                         
-{/**                                                                                                                                    
-    * Return something otherwise return 0                                                                                               
-    */                                                                                                                                  
+{
+  /**                                                                                                                                    
+   * Return something otherwise return 0                                                                                               
+   */                                                                                                                                  
                                                                                                                                         
-        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                 
+       printf("with proper grammar, but the outcome is a piece of art,\n");                                                                 
                                                                                                                                         
-        return (0);                                                                                                                     
+       return (0);                                                                                                                     
                                                                                                                                         
 }                      
