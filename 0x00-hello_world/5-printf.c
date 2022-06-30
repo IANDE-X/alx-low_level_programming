379a382eb2204013148e444d091e299df876134e
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-01 00:25:08 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
-/**                                                                                                                           
- * main - Prints "with proper grammar, but the outcome is a piece of art,"                                                                                                                                                                                              
- * *Return: Always 0 (success)                                                                                                         
- */                                                                                                                                                                                                                                                                        
-int main(void)                                                                                                                          
-                                                                                                                                        
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Good)
+ */
+int main(void)
 {
-  /**                                                                                                                                    
-   * Return something otherwise return 0                                                                                               
-   */                                                                                                                                  
-                                                                                                                                        
-   printf("with proper grammar, but the outcome is a piece of art,\n");                                                                 
-                                                                                                                                        
-   return (0);                                                                                                                     
-                                                                                                                                        
-}                      
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
