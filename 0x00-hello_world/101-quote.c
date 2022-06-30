586b7aa3f6401a7a7d3898921b68701130090255
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-01 01:03:28 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 1 (Error)
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Always 0 (sucess)
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
