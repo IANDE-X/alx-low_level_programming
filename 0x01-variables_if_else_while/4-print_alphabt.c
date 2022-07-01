c7db8ead5682d59558d83618910d5fe3c1433f9e
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-01 23:19:48 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,20 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
- * main - Entry point
- *
- * Description: Alphabet except some
- *
- * Return: Always 0 (Success)
- */
-
+/**
+* main - Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char alpha;
+		char letter;
 
-  for (alpha = 'a'; alpha <= 'z'; alpha++)
-{
-if (alpha != 'e' && alpha != 'q')
-  putchar(alpha);
-}
-putchar('\n');
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		if (letter != 'e' && letter != 'q')
+		putchar(letter);
+		}
+		putchar('\n');
 
-return (0);
+	return (0);
 }
