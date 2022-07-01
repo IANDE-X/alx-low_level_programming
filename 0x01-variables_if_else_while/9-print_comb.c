e4fc7ce8a1a27daaff640f7673cc6fd9f2ea4162
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-02 00:19:48 +0200

    Update 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints all possible combinations of single-digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c <= 9; c++)
+{
+putchar(c + '0');
+if (c < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
