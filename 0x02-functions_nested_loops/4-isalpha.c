1604d95a1d7c828654b5ea965d958796a3fa3e9d
IANDE-X <mwenesiian@gmail.com>
2022-07-04 21:47:18 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+       if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) 
+
+		return (1);
+	else
+		return(0);
+
+}
