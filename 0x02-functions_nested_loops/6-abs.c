9afaa984c6547c2ce9c57a6d041b157e5f68159b
IANDE-X <mwenesiian@gmail.com>
2022-07-04 21:58:11 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+nclude "main.h"
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return(0);
+}
