d9b4a590838e641d9dec01b144a4380b478d23f4
IANDE-X <mwenesiian@gmail.com>
2022-07-05 12:26:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,14 +10,9 @@ int main(void)
 
 {
 	char alpha;
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
-	
+	for (alpha = 'a'; alpha <= 'z'; alpha++)	
 	{
 		_putchar(alpha);
-
-
-
-
 	}
 	_putchar('\n');
 }
