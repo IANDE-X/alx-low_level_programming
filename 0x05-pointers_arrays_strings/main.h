bfae5b9849e7db6d55bad75d96150e7cf4de8a13
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-11 13:59:54 +0200

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -13,4 +13,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+#endif /* MAIN_H */
