228607ddf1d7df4152ebc8281e139871b48f4ad4
Iam Ian <74612008+IANDE-X@users.noreply.github.com>
2022-07-24 23:09:58 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar rc liball.a *.o
-=======
-#/bin/bash
-gcc -c *.c
-ar rc liball.a *.o
->>>>>>> a395130b8bf9fb7000ce0587d27b31e3763394db
