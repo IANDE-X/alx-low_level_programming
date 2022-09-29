769b00c6d0f6b75a4fed9d389b86512917f3ff13
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-29 20:36:02 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,54 +1,52 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Add node at nth index
- *
- * @h: Head of node
- *
- * @idx: index
- *
- * @n: struct int
- *
- * Return: dlistint_t
- */
-
+ * insert_dnodeint_at_index - inserts a node node at a given position
+ * in a dlistint_t list.
+ * @h: pointer to the list.
+ * @idx: position to add the node.
+ * @n: data for the new node.
+ * Return: the address of the new node, or NULL if it failed
+ **/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *current;
-	unsigned int count = 0;
+	dlistint_t *aux_node = *h, *new_node;
+	unsigned int index, cont = 0;
 
-	if (h == NULL || new_node == NULL)
-	{
+	/* create node */
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
 	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	current = *h;
 
+	/* border case for insert at the beginning */
 	if (idx == 0)
 	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
+		new_node->prev = NULL;
+		new_node->next = *h;
+		if (*h)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (*h);
 	}
-	while (current)
+
+	/* search of position to insert */
+	index = idx - 1;
+	while (aux_node && cont != index)
 	{
-		if (current->next == NULL && count == idx - 1)
-		{
-			new_node = add_dnodeint_end(h, n);
-			return (new_node);
-		}
-		else if ((idx - 1) == count)
-		{
-			new_node->next = current->next;
-			new_node->prev = current;
-			current->next->prev = new_node;
-			current->next = new_node;
-			return (new_node);
-		}
-		count++;
-		current = current->next;
+		cont++;
+		aux_node = aux_node->next;
+	}
+
+	/* general case */
+	if (cont == index && aux_node)
+	{
+		new_node->prev = aux_node;
+		new_node->next = aux_node->next;
+		if (aux_node->next)
+			aux_node->next->prev = new_node;
+		aux_node->next = new_node;
+		return (new_node);
 	}
 	free(new_node);
 	return (NULL);
