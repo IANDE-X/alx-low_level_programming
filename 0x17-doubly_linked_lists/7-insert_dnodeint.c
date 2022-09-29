c9ab163c82f65afb961f227eae4176df9e9080e3
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-29 16:58:12 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,102 +1,48 @@
 #include "lists.h"
-
-void add_in_middle(dlistint_t *node_at_choice_index, dlistint_t *new_node);
-void add_at_the_end(dlistint_t *node_b4_null, dlistint_t *new_node);
-dlistint_t *create_new_node(int n);
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: double pointer to the head of the list
- * @idx: index of the list where the new node should be added
- * @n: elemenent of the new node to be added at @idx
- * Return: the address of the new node, or NULL if it failed
+ * @h: double pointer to the beginning of the linked list
+ * @idx: index at which to insert the new node
+ * @n: data to enter into new node
+ *
+ * Return: pointer to the new node, or NULL on failure
  */
-
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current_node_at_the_index = NULL, *new_node, *c_node, *prev;
-	unsigned int i = 0;
+	dlistint_t *new, *next, *current;
+	unsigned int i;
 
-	if (!(*h))
+	if (h == NULL)
 		return (NULL);
-
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	c_node = *h;
-
-	while (c_node)
+	if (idx != 0)
 	{
-		if (i == idx)
-		{
-			current_node_at_the_index = c_node;
-			break;
-		}
-
-		prev = c_node;
-		c_node = c_node->next;
-		i++;
+		current = *h;
+		for (i = 0; i < idx - 1 && current != NULL; i++)
+			current = current->next;
+		if (current == NULL)
+			return (NULL);
 	}
-
-	if (!current_node_at_the_index && (i != idx))
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
 		return (NULL);
-
-	new_node = create_new_node(n);
-
-	if (!new_node)
-		return (NULL);
-
-	if (!current_node_at_the_index && (i == idx))
-		add_at_the_end(prev, new_node);
+	new->n = n;
+	if (idx == 0)
+	{
+		next = *h;
+		*h = new;
+		new->prev = NULL;
+	}
 	else
-		add_in_middle(current_node_at_the_index, new_node);
-
-	return (new_node);
-}
-
-/**
- * add_in_middle - adds a node in between nodes of dlistint_t list
- * @node_at_choice_index: pointer to the node at index of insertion
- * @new_node: pointer to new node
- */
-
-void add_in_middle(dlistint_t *node_at_choice_index, dlistint_t *new_node)
-{
-	new_node->prev = node_at_choice_index->prev;
-	new_node->next = node_at_choice_index;
-
-	(node_at_choice_index->prev)->next = new_node;
-	node_at_choice_index->prev = new_node;
-}
-
-/**
- * add_at_the_end - adds a node to end of dlistint_t list
- * @node_b4_null: pointer to node before end null node
- * @new_node: pointer to new node
- */
-
-void add_at_the_end(dlistint_t *node_b4_null, dlistint_t *new_node)
-{
-	node_b4_null->next = new_node;
-	new_node->prev = node_b4_null;
-	new_node->next = NULL;
-}
-
-/**
- * create_new_node - creates a new node for dlistint_t list
- * @n: elemenent of the new node
- * Return: pointer to a new node
- */
-
-dlistint_t *create_new_node(int n)
-{
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-
-	return (new_node);
+	{
+		new->prev = current;
+		next = current->next;
+		current->next = new;
+	}
+	new->next = next;
+	if (new->next != NULL)
+		new->next->prev = new;
+	return (new);
 }
