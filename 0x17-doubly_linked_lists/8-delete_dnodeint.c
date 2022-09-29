31ea4bca3869ca2d01054557170babee34a49a5a
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-29 18:09:23 +0200

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,55 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a dlistint_t list.
- * @head: pointer to the list.
- * @index: position of the node to delete.
- * Return: 1 if it succeeded, -1 if it failed.
- **/
+ * delete_dnodeint_at_index - Delete node at nth index
+ *
+ * @head: Head of node
+ *
+ * @index: index
+ *
+ * Return: 1 succeed, -1 if fail
+ */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *aux_node = *head;
-	dlistint_t *node_to_delete = *head;
-	unsigned int idx;
-	unsigned int cont = 0;
+	dlistint_t *node;
+	unsigned int count;
 
-	/* border case for empty list */
-	if (!(*head))
+	if (*head == NULL)
 		return (-1);
 
-	/* border case for delete at the beginning */
+	node = *head;
 	if (index == 0)
 	{
-		*head = node_to_delete->next;
-		free(node_to_delete);
-		if (*head)
-			(*head)->prev = NULL;
+		*head = node->next;
+		if (node->next != NULL)
+		{
+			node->next->prev = NULL;
+		}
+		free(node);
 		return (1);
 	}
-
-	/* search of position to delete */
-	idx = index - 1;
-	while (aux_node && cont != idx)
+	for (count = 0; node != NULL && count < index - 1 ; count++)
 	{
-		cont++;
-		aux_node = aux_node->next;
+		node = node->next;
+	}
+	if (node == NULL || node->next == NULL)
+	{
+		return (-1);
 	}
 
-	/* general case */
-	if (cont == idx && aux_node)
+	if (node->next->next != NULL)
 	{
-		node_to_delete = aux_node->next;
-		if (node_to_delete->next)
-		node_to_delete->next->prev = aux_node;
-		aux_node->next = node_to_delete->next;
-		free(node_to_delete);
+		node->next = node->next->next;
+		free(node->next->prev);
+		node->next->prev = node;
 		return (1);
 	}
-
+	else
+	{
+		free(node->next);
+		node->next = NULL;
+		return (1);
+	}
 	return (-1);
 }
