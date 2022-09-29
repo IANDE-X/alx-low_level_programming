5e47ae4713a91355f4212056fd91e460fc65b054
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-09-29 20:27:25 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,55 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Add node at nth index
- *
- * @h: Head of node
- *
- * @idx: index
- *
- * @n: struct int
- *
- * Return: dlistint_t
+ * insert_dnodeint_at_index - insert node at specific index
+ * @h: head of linked list
+ * @idx: index of new node
+ * @n: new node value
+ * Return: inserted node
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *current;
-	unsigned int count = 0;
+	dlistint_t *new;
 
-	if (h == NULL || new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+	if (h == NULL)
+		return (0);
+
 	current = *h;
 
-	if (idx == 0)
+	while (idx != 0)
 	{
-		new_node = add_dnodeint(h, n);
-		return (new_node);
-	}
-	while (current)
-	{
-		if (current->next == NULL && count == idx - 1)
-		{
-			new_node = add_dnodeint_end(h, n);
-			return (new_node);
-		}
-		else if ((idx - 1) == count)
-		{
-			new_node->next = current->next;
-			new_node->prev = current;
-			current->next->prev = new_node;
-			current->next = new_node;
-			return (new_node);
-		}
-		count++;
 		current = current->next;
+		idx--;
+		if (current == NULL)
+			return (NULL);
 	}
-	free(new_node);
-	return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = current;
+	new->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = new;
+
+	/*TODO: Handle special case when idx is 0 and last index*/
+
+	return (current);
 }
+
