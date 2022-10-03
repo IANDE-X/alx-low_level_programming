cdc86805d3cedeeb9420e328ddd92c70ba0f1614
The Ian <74612008+IANDE-X@users.noreply.github.com>
2022-10-03 23:17:33 +0200

    Update 0-main.c

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-#include "main.h"
-
-#include <stdio.h>
-
-
-
-/**
-
-   * main - check the code
-
-    *
-
-     * Return: Always EXIT_SUCCESS.
-
-      */
-
-int main(void)
-
-{
-
-	    printf("%d\n", _strlen("My Dyn Lib"));
-
-	        return (EXIT_SUCCESS);
-
-}
-=======
 #include "main.h"
 #include <stdio.h>
 
@@ -38,4 +11,3 @@ int main(void)
     printf("%d\n", _strlen("My Dyn Lib"));
     return (EXIT_SUCCESS);
 }
->>>>>>> 57396b7b5671a796e30c5cce92a107459afbd1f1
